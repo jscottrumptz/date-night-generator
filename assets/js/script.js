6065fe18aae3ebf99a58afd1b1faf8ba4302289b
J Scott Rumptz <jscott@rumptz.tech>
2021-01-25 12:34:14 -0600

    modal open and close

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+$("#meal-detail").on("click", function(event){
+    $("#modal").addClass("is-active");
+});
+
+$("#modal-close").on("click", function(event){
+    $("#modal").removeClass("is-active");
+});
